21056214464133f32c2cb7a40c2e8a54867a21fc
Johanna Geßner <johanna-gessner@web.de>
2023-08-11 09:40:55 +0200

    display player in the center

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 class Player {
     constructor(){
-        this.positionX = 50;
-        this.positionY = 0;
         this.width = 20;
         this.height = 10;
+        this.positionX = 50 - (this.width / 2);
+        this.positionY = 0;
         this.domElement = null;
 
         this.createDomElement();
@@ -36,10 +36,10 @@ class Player {
 
 class Obstacle {
     constructor(){
-        this.positionX = 50;
-        this.positionY = 100;
         this.width = 20;
         this.height = 10;
+        this.positionX = 50;
+        this.positionY = 100;
         this.domElement = null;
 
         this.createDomElement();
